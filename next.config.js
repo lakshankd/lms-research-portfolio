c1881a5c437bf4f3c0cc7685c35acdb30cdbc4b9
Dominic Lakshan <dominiclakshan831@gmail.com>
2025-05-08 13:02:23 +0530

    initializing project

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/lms-research-portfolio",
+  output: "export",
+  reactStrictMode: false,
+};
+
+module.exports = nextConfig;
